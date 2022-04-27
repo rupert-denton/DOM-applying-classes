9092dd46986e6643f684b2974597919bf75d8d76
rupert-denton <rupertdenton1@gmail.com>
2022-04-27 15:05:45 +1200

    finished dot colouring game

--- classes.js
+++ classes.js
@@ -1,16 +1,18 @@
-// Don't change or delete this line! It waits until the DOM has loaded, then calls 
-// the start function. More info: 
+// Don't change or delete this line! It waits until the DOM has loaded, then calls
+// the start function. More info:
 // https://developer.mozilla.org/en-US/docs/Web/Events/DOMContentLoaded
 document.addEventListener('DOMContentLoaded', start)
 
-function start () {
+function start() {
   // The first example is done for you. Uncomment the line below and reload the browser.
-  // one()
-    
+  one()
+  two()
+  three()
+  makeVisible()
   // Your turn! Create a new function called `two`, then call it from here.
 }
 
-function one () {
+function one() {
   // First, we have to find the element:
   var one = document.getElementById('one')
 
@@ -19,7 +21,25 @@ function one () {
 }
 
 // CREATE FUNCTION two HERE
+function two() {
+  // First, we have to find the element:
+  var two = document.getElementById('two')
+
+  // Next, we apply a new CSS class to it:
+  two.classList.add('green')
+}
 
 // CREATE FUNCTION three HERE
+function three() {
+  // First, we have to find the element:
+  var three = document.getElementById('three')
+
+  // Next, we apply a new CSS class to it:
+  three.classList.add('red')
+}
 
 // CREATE FUNCTION makeVisible HERE
+function makeVisible() {
+  var element = document.getElementsByClassName('invisible')[0]
+  element.classList.add('visible')
+}
